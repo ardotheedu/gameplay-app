36ea48c1e127b94275972a5ab22a0dbb61401f92
ardotheedu <eduardosalima3@gmail.com>
2021-06-25 11:59:33 -0300

    refactor: small fixs in the SignIn and Add fonts

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -12,12 +12,6 @@ import { styles } from './styles'
 export function SignIn() {
   return (
     <View style={styles.container}>
-       <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {/* Vai deixar a barra de status da mesma cor que a do fundo */}
       <Image
         source={IllustrationImg}
         style={styles.image}
@@ -26,9 +20,9 @@ export function SignIn() {
 
       <View style={styles.content} >
           <Text style={styles.title}>
-              Organize {'\n'}
-              suas jogatinas {'\n'}
-              facilmente
+              Conecte-se {'\n'}
+              e organize suas {'\n'}
+              jogatinas
           </Text>
 
           <Text style={styles.subtitle}>
